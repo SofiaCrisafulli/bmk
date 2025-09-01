d6b3eb54485d15ec3f1bc3093fc1c8c02ca9eedd
SofiaCrisa <71233317+SofiaCrisafulli@users.noreply.github.com>
2025-09-01 11:08:11 -0300

    tre

--- custom/addons/stock_3d_view-16.0.1.0.0/stock_3d_view/models/stock_location.py
+++ custom/addons/stock_3d_view-16.0.1.0.0/stock_3d_view/models/stock_location.py
@@ -1,80 +1,51 @@
-"""This module inherits stock.location model."""
 # -*- coding: utf-8 -*-
-#############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
-#
-#    You can modify it under the terms of the GNU LESSER
-#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
-#
-#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
-#    (LGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
 from odoo import fields, models
 
 
 class StockLocation(models.Model):
-    """Class for adding fields to stock.location"""
+    """Extend stock.location with 3D fields."""
     _inherit = 'stock.location'
 
-    length = fields.Float(string="Length (M)",
-                          help="Length of the location in meters")
-    width = fields.Float(string="Width (M)",
-                         help="Width of the location in meters")
-    height = fields.Float(string="Height (M)",
-                          help="Height of the location in meters")
-    pos_x = fields.Float(string="X (in px)",
-                         help="Position of the location along X-axis")
-    pos_y = fields.Float(string="Y (in px)",
-                         help="Position of the location along Y-axis")
-    pos_z = fields.Float(string="Z (in px)",
-                         help="Position of the location along Z-axis")
-    unique_code = fields.Char(string="Location Code",
-                              help="Unique code of the location")
-    max_capacity = fields.Integer(string="Capacity (Units)",
-                                  help="Maximum capacity of the location in "
-                                       "terms of Units")
+    length = fields.Float(string="Length (M)", help="Length of the location in meters")
+    width = fields.Float(string="Width (M)", help="Width of the location in meters")
+    height = fields.Float(string="Height (M)", help="Height of the location in meters")
+    pos_x = fields.Float(string="X (in px)", help="Position of the location along X-axis")
+    pos_y = fields.Float(string="Y (in px)", help="Position of the location along Y-axis")
+    pos_z = fields.Float(string="Z (in px)", help="Position of the location along Z-axis")
+    unique_code = fields.Char(string="Location Code", help="Unique code of the location")
+    max_capacity = fields.Integer(string="Capacity (Units)", help="Maximum capacity of the location in terms of Units")
 
     _sql_constraints = [
-        ('unique_code', 'UNIQUE(unique_code)',
-         "The location code must be unique per company !"),
+        ('unique_code', 'UNIQUE(unique_code)', "The location code must be unique!"),
     ]
 
-   def action_view_location_3d_button(self):
-    self.ensure_one()
-    # Queremos SOLO las ubicaciones con el mismo padre (location_id) que la actual.
-    if self.location_id:
-        # La actual tiene padre -> mostrar a todos los que comparten ese padre (hermanos + la actual)
-        domain = [
-            ('usage', '=', 'internal'),
-            ('location_id', '=', self.location_id.id),
-        ]
-    else:
-        # La actual es raíz -> mostrar solo las raíces (location_id = False)
-        domain = [
-            ('usage', '=', 'internal'),
-            ('location_id', '=', False),
-        ]
+    def action_view_location_3d_button(self):
+        self.ensure_one()
+        # Solo ubicaciones con el mismo padre (location_id) que la actual:
+        if self.location_id:
+            # La actual tiene padre → mostrar hermanos (incluida la actual)
+            domain = [
+                ('usage', '=', 'internal'),
+                ('location_id', '=', self.location_id.id),
+            ]
+            parent_location_id = self.location_id.id
+        else:
+            # La actual es raíz → mostrar solo raíces (location_id = False)
+            domain = [
+                ('usage', '=', 'internal'),
+                ('location_id', '=', False),
+            ]
+            parent_location_id = False
 
-    return {
-        'type': 'ir.actions.client',
-        'tag': 'open_form_3d_view',
-        'name': 'Mapa 3D',
-        'context': {
-            'location_domain': domain,
-            'parent_location_id': self.location_id.id if self.location_id else False,
-            'loc_id': self.id,                # por si tu RPC lo usa
-            'company_id': self.company_id.id,
-        },
-        'target': 'current',
-    }
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'open_form_3d_view',
+            'name': 'Mapa 3D',
+            'context': {
+                'location_domain': domain,
+                'parent_location_id': parent_location_id,
+                'loc_id': self.id,            # por si tu RPC lo usa
+                'company_id': self.company_id.id,
+            },
+            'target': 'current',
+        }
